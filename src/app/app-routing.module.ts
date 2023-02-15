4a9cf69eb22ca8a629ec882f9771dcd981cc16cf
Leonardo <leonardogeminiani7@gmail.com>
2023-02-15 14:03:36 +0100

    home html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AutoComponent } from './components/auto/auto.component';
+import { EditComponent } from './components/edit/edit.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'auto', component: AutoComponent},
+  {path: 'edit', component: EditComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
