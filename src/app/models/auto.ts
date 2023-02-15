b66a07eb59f9dccecfac1c915b03b16265234c21
Leonardo <leonardogeminiani7@gmail.com>
2023-02-15 16:07:37 +0100

    auto service

---
+++ src/app/models/auto.ts
@@ -0,0 +1,20 @@
+export enum CategoriaAuto {
+    Suv,
+    Berlina,
+    Van,
+    Cabrio,
+    Supercar,
+    Bassocosto,
+}
+
+export class Auto
+{
+    constructor (
+        public id: number,
+        public marca: string,
+        public modello: string,
+        public categoria: CategoriaAuto,
+        public prezzo: number,
+        public immagine: string
+    ){}
+}
--- src/app/services/auto.service.ts
+++ src/app/services/auto.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from "@angular/core";
+import { Auto, CategoriaAuto } from "../models/auto";
 
 @Injectable()
 export class AutoService {
-    
+    autoList: Auto[] = [];
+
+    constructor(){
+        this.autoList = [
+        ]   
+    }
+
+    get auto() {
+        return this.autoList;
+    }
+
+    RemoveAuto(id: CategoriaAuto) {
+        this.autoList = this.autoList.filter(el => el.id !== id);
+    }
 }
